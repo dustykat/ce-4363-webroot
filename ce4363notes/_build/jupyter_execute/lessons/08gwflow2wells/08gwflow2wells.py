0884eb65ec09c6b8b3915a50d02abdd9ef36658a
theodore cleveland <ted.cleveland@gmail.com>
2023-02-08 20:53:53 +0000

    update1d solns

--- ce4363notes/_build/jupyter_execute/lessons/08gwflow2wells/08gwflow2wells.py
+++ ce4363notes/_build/jupyter_execute/lessons/08gwflow2wells/08gwflow2wells.py
@@ -7,9 +7,14 @@
 # 
 # - Drawdown
 # - Cone of Depression
+# - Well interference
 # 
 # 
 # ## Unsteady Flow Solutions
+
+# ## References
+# 
+# 1.  [Cleveland, T.G., (2001) *Groundwater Hydrology - Lecture 009* Lecture Notes to accompany CE 6361 at the University of Houston](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/08gwflow2wells/Lecture009.PDF)
 
 # In[ ]:
 
