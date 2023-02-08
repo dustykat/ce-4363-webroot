2e42394bfa7d3df7636068466ea93727cd9cd61a
theodore cleveland <ted.cleveland@gmail.com>
2023-02-08 20:37:22 +0000

    update1d solns

---
+++ ce4363notes/_build/jupyter_execute/lessons/07gwflow1Dsolutions/07gwflow1Dsolutions.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Groundwater Flow Solutions
+# 
+# ## Steady Flow 1D Cartesian
+# 
+# ### Confined Aquifer (pp. 143-145)
+# 
+# ![](cf1d001.png)
+# 
+# ![](cf1d002.png)
+# 
+# ![](cf1d003.png)
+# 
+# ![](cf1d004.png)
+# 
+# ![](cf1d005.png)
+# 
+# 1D flow in a confined aquifer can be solved by direct application of continunity and Darcy's law.  For example consider:
+# 
+# ### Unconfined Aquifer (pp. 145-151)
+# 
+# ![](uf1d001.png)
+# 
+# ![](uf1d002.png)
+# 
+# ![](uf1d003.png)
+# 
+# ![](uf1d004.png)
+# 
+# ![](uf1d005.png)
+# 
+# ![](uf1d006.png)
+# 
+# 1D flow in an unconfined aquifer can be solved by direct application of continunity and Darcy's law.  For example consider:
+# 
+# ![](unconfined-with-recharge-001.png)
+# 
+# **Solution Sketch**
+# 
+# Apply continunity (or actually the unconfined flow equation)
+# 
+# ![](unconfined-with-recharge-002.png)
+# 
+# Then analysis (e.g. calculus) to find the result sought.
+# 
+# ![](unconfined-with-recharge-003.png)
+# 
+# **Another similar example:**
+# 
+# ![](unconfined-with-recharge2-001.png)
+# 
+# ![](unconfined-with-recharge2-002.png)
+# 
+# ![](unconfined-with-recharge2-003.png)
+# 
+# ![](unconfined-with-recharge2-004.png)
+
+# ## Steady Flow 1D Cylindrical
+# 
+# 
+# ### Confined Aquifer (pp. 172-174)
+# 
+# 1D radial in a confined aquifer can be solved by direct application of continunity and Darcy's law.  For example consider:
+# 
+# ![](cfwell001.png)
+# 
+# ![](cfwell002.png)
+# 
+# ![](cfwell003.png)
+# 
+# ![](cfwell004.png)
+# 
+# ![](cfwell005.png)
+# 
+# ![](cfwell006.png)
+# 
+# ![](cfwell007.png)
+# 
+# ![](cfwell008.png)
+# 
+# ![](cfwell009.png)
+# 
+# 
+# ### Unconfined Aquifer (pp. 175)
+# 
+# 1D radial in unconfined aquifer can be solved by direct application of continunity and Darcy's law.  For example consider:
+# 
+# ![](unwell001.png)
+# 
+# ![](unwell002.png)
+# 
+# ![](unwell003.png)
+# 
+# ![](unwell004.png)
+# 
+
+# 
+
+# ## References 
+# 
+# 1. [Cleveland, T.G., (2001) *Groundwater Hydrology - Lecture 008* Lecture Notes to accompany CE 6361 at the University of Houston](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/06gwflowequations/Lecture_008.PDF)
+
+# In[ ]:
+
+
+
+
