662097b804a30c1850b589f8cd7682d55db26159
theodore cleveland <ted.cleveland@gmail.com>
2023-02-20 23:38:51 +0000

    upload hantush

---
+++ ce4363notes/_build/jupyter_execute/lessons/10gwflow2wells/10gwflow2wells.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Groundwater Flow to Wells - III
+# 
+# ## Image Wells
+# 
+# ## Aquifer Pumping Tests
+# 
+# ![](aquifertests.png)
+# 
+# ### Specific Capacity (pp. 211-212)
+# 
+# ![](specCapacity1.png)
+# 
+# ![](specCapacity2.png)
+# 
+# ### Pumping Tests
+# 
+# Different interpretation based on suspected aquifer structure.
+# 
+# ![](aqpump1.png)
+
+# #### Type Curve Analysis (pp. 172-179)
+# 
+# ![](typecurve1.png)
+# 
+# ![](typecurve2.png)
+# 
+# ![](typecurve3.png)
+# 
+# **Example**
+# 
+# ![](example1-1.png)
+# 
+# ![](example1-2.png)
+# 
+# ![](example1-3.png)
+# 
+# ![](example1-4.png)
+# 
+# This example also appears in [Cleveland, T.G., 1996. ”Type-Curve Matching using a Computer Spreadsheet,”
+# Ground Water, Vol. 34, No. 3.](http://54.243.252.9/about-me-webroot/about-me/MyWebPapers/journal_papers/type_curve_jgw_1996/type_curve_jgw_1996.pdf)
+# 
+# Here is a blank [Theis Type Curve](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/10gwflow2wells/theistypecurve.png)
+# 
+# 
+
+# #### Cooper-Jacob Approximation (pp. 179-183)
+# 
+# ![](cooper1.png)
+# 
+# ![](cooper2.png)
+# 
+# ![](cooper3.png)
+
+# #### Hantush Leaky Aquifer Analysis (pp. 185-189)
+# 
+# ![](walton1.png)
+# 
+# ![](walton2.png)
+# 
+# **Example**
+# 
+# ![](walton3.png)
+# 
+# ![](walton4.png)
+# 
+# ![](walton5.png)
+# 
+# ![](walton6.png)
+# 
+# This example also appears in [Cleveland, T.G., 1996. ”Type-Curve Matching using a Computer Spreadsheet,”
+# Ground Water, Vol. 34, No. 3.](http://54.243.252.9/about-me-webroot/about-me/MyWebPapers/journal_papers/type_curve_jgw_1996/type_curve_jgw_1996.pdf)
+# 
+# Here is a blank [Walton's Type Curve](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/10gwflow2wells/waltonscurve.png)
+
+# ### Slug Tests (pp. 195-211)
+# 
+# [Slug Test wikipedia](https://en.wikipedia.org/wiki/Slug_test)
+# 
+# **Hvorslov's Approach**
+# 
+# ![](slug1.png)
+# 
+# ![](slug2.png)
+# 
+# ![](slug3.png)
+# 
+# ![](slug4.png)
+# 
+# ![](slug5.png)
+# 
+# ![](slug6.png)
+# 
+# The spreadsheet above is located at [Slugtest_001.xls](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/10gwflow2wells/Slugtest_001.xls)
+# 
+# **Nugyen and Pinder Approach**
+# 
+# ![](np1.png)
+# 
+# ![](np2.png)
+# 
+# ![](np3.png)
+# 
+# 
+
+# 
+
+# ## References
+# 
+# 1. [Cleveland, T.G., 1996. ”Type-Curve Matching using a Computer Spreadsheet,”
+# Ground Water, Vol. 34, No. 3.](http://54.243.252.9/about-me-webroot/about-me/MyWebPapers/journal_papers/type_curve_jgw_1996/type_curve_jgw_1996.pdf)
+# 
+# 2. [Cleveland, T.G., 1996. ”Type-Curve Matching using a Computer Spreadsheet,”
+# Ground Water, Vol. 34, No. 3. (Spreadsheets in the Document (XLS))](http://54.243.252.9/about-me-webroot/about-me/MyWebPapers/journal_papers/type_curve_jgw_1996/)
+# 
+# 3. [Hvorslev, 1951. "Time Lag and Soil Permeability in Ground Water Observations", Bulletin No. 36, U.S. Army Corps of Engineers p. 50.](https://health.hawaii.gov/heer/files/2021/07/Hvorslev1951.pdf)
+# 
+# 4. [Cleveland, T.G., (2001) *Groundwater Hydrology - Lecture 010* Lecture Notes to accompany CE 6361 at the University of Houston](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/10gwflow2wells/Lecture010.PDF)
+# 
+# 
+
+# In[ ]:
+
+
+
+
