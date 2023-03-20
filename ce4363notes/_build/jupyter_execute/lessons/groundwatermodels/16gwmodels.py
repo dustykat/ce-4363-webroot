97974b13a665102d0c39190b0fec78cd83eb9dd6
theodore cleveland <ted.cleveland@gmail.com>
2023-03-20 19:28:48 +0000

    regional flow

--- ce4363notes/_build/jupyter_execute/lessons/groundwatermodels/16gwmodels.py
+++ ce4363notes/_build/jupyter_execute/lessons/groundwatermodels/16gwmodels.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Regional Flow Models
+# # Regional Flow 
+# 
+# ## Recharge and Discharge Areas
+# 
+# ## Flow Patterns in Aquifers
+# 
+# ### Vertical Slices
+# 
+# ### Horizontal Slides
+# 
+# ## Low Permeability Inclusions
+# 
+# ## High Permeability Inclusions
+# 
+# ## Heterogenity and Anisotropy
+# 
+# ## References
+# 
+# 1. [Bravo,R., Rogers, J.R., and T.G. Cleveland, 1996. *Modeling Groundwater Flow using Flux Boundary Conditions.* Water Resources Bulletin, Vol 32., No. 1, pp 39-46](http://54.243.252.9/about-me-webroot/about-me/MyWebPapers/journal_papers/AWRA_WRB_1996/AWRA_WRB_1996.pdf)
 
 # In[1]:
 
