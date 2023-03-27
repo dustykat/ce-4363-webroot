32c05b0f9f41dd054548dce4da3e6bb7bba46bdb
theodore cleveland <ted.cleveland@gmail.com>
2023-03-27 19:55:01 +0000

    update

--- ce4363notes/_build/jupyter_execute/lessons/16recharge.py
+++ ce4363notes/_build/jupyter_execute/lessons/16recharge.py
@@ -2,3 +2,9 @@
 # coding: utf-8
 
 # # Recharge
+# 
+# ## References
+# 
+# 1. [Unsaturated Zone and Recharge](http://54.243.252.9/ce-4363-webroot/3-Readings/Lecture013.PDF)
+# 2. [Measuring Soil Properties](http://54.243.252.9/ce-4363-webroot/3-Readings/Lecture014.PDF)
+# 3. [Water Profiles During Precipitation](http://54.243.252.9/ce-4363-webroot/3-Readings/Lecture015.PDF)
--- ce4363notes/_build/jupyter_execute/lessons/17fieldmethods.py
+++ ce4363notes/_build/jupyter_execute/lessons/17fieldmethods.py
@@ -2,3 +2,16 @@
 # coding: utf-8
 
 # # Field Methods
+# 
+# 
+
+# ## References
+# 
+# 1. [Seismic Refreaction Techniques](http://54.243.252.9/ce-4363-webroot/3-Readings/ApplicationofSeismic-RefractionTechniquestoHydrologicStudies.pdf)
+# 2. [Logging Techniques](http://54.243.252.9/ce-4363-webroot/3-Readings/Lecture011.PDF)
+
+# In[ ]:
+
+
+
+
